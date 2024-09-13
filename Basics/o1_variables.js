8c13cf0e2d667404c61979c15522a0dc8964aa75
mehbub17 <70087689+mehbub17@users.noreply.github.com>
2024-09-13 18:49:04 +0000

    sdfsf

---
+++ Basics/o1_variables.js
@@ -0,0 +1,14 @@
+
+const accountId = 14489
+
+let accountEmail = "hussianmehbub@gmail.com"
+
+var accountPassword = "12234"
+
+accountCity = "Assam"
+
+let accountState;
+
+console.log(accountEmail)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
